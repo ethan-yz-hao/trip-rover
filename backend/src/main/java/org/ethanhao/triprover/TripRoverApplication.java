83ab57176aab141f07d11e6202daf3edbd9b4574
Ethan Hao <hao.yz.ethan@gmail.com>
2024-05-21 14:43:08 -0400

    init springboot backend

---
+++ backend/src/main/java/org/ethanhao/triprover/TripRoverApplication.java
@@ -0,0 +1,13 @@
+package org.ethanhao.triprover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripRoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripRoverApplication.class, args);
+	}
+
+}
