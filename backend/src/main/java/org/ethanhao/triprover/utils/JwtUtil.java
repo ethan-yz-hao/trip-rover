4ee013d3a5cd4d2001d38e9c57e9ad723f563f58
Ethan Hao <hao.yz.ethan@gmail.com>
2024-05-24 14:53:58 -0400

    jwt utils class

--- backend/src/main/java/org/ethanhao/triprover/utils/JwtUtil.java
+++ backend/src/main/java/org/ethanhao/triprover/utils/JwtUtil.java
@@ -18,7 +18,7 @@ public class JwtUtil {
     //有效期为
     public static final Long JWT_TTL = 60 * 60 * 1000L;// 60 * 60 *1000 一个小时
     //设置秘钥明文
-    public static final String JWT_KEY = "sangeng";
+    public static final String JWT_KEY = "ethanhao";
 
     public static String getUUID() {
         String token = UUID.randomUUID().toString().replaceAll("-", "");
@@ -82,7 +82,7 @@ public class JwtUtil {
     }
 
     public static void main(String[] args) throws Exception {
-        String token ="eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJjYWM2ZDVhZi1mNjVlLTQ0MDAtYjcxMi0zYWEwOGIyOTIwYjQiLCJzdWIiOiJzZyIsImlzcyI6InNnIiwiaWF0IjoxNjM4MTA2NzEyLCJleHAiOjE2MzgxMTAzMTJ9.JVsSbkP94wuczb4QryQbAke3ysBDIL5ou8fWsbt_ebg ";
+        String token = createJWT("hello");
         Claims claims = parseJWT(token);
         System.out.println(claims);
     }
