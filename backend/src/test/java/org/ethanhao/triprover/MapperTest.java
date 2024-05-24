4c73b13704afaadede0ed6a628eefc71c748ccd0
Ethan Hao <hao.yz.ethan@gmail.com>
2024-05-24 14:45:19 -0400

    implement password encoder

--- backend/src/test/java/org/ethanhao/triprover/MapperTest.java
+++ backend/src/test/java/org/ethanhao/triprover/MapperTest.java
@@ -4,12 +4,22 @@ import org.ethanhao.triprover.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 public class MapperTest {
 
     @Autowired
     private UserMapper userMapper;
+
+    @Test
+    public void testBCrypt() {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        System.out.println(encoder.encode("password"));
+        System.out.println(encoder.encode("password"));
+        System.out.println(encoder.matches("password",
+                "$2a$10$NEgAp4igmIoz6iOUTGB34ehY0aHNHblMZOobvmT44Pyq9rVuiwRlS"));
+    }
 
     @Test
     public void testSelect() {
