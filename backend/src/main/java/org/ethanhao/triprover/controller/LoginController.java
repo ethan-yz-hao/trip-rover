0f126e07cd7af4216064c421df076273b5556663
Ethan Hao <hao.yz.ethan@gmail.com>
2024-09-29 15:47:47 -0400

    update user endpoint url

--- backend/src/main/java/org/ethanhao/triprover/controller/LoginController.java
+++ backend/src/main/java/org/ethanhao/triprover/controller/LoginController.java
@@ -8,30 +8,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/user")
 public class LoginController {
 
     @Autowired
     private LoginService loginService;
 
-    @PostMapping("/user/login")
+    @PostMapping("/login")
     public ResponseResult login(@RequestBody User user, HttpServletResponse response) {
         return loginService.login(user, response);
     }
 
-    @PostMapping("/user/logout")
+    @PostMapping("/logout")
     public ResponseResult logout(HttpServletResponse response) {
         return loginService.logout(response);
     }
 
-    @PostMapping("/user/register")
+    @PostMapping("/register")
     public ResponseResult register(@RequestBody User user) {
         return loginService.register(user);
     }
 
-    @PostMapping("/user/update")
+    @PostMapping("/update")
     @PreAuthorize("hasAuthority('system:user:update')")
     public ResponseResult updatePassword(@RequestBody User user) {
         return loginService.updateUser(user);
