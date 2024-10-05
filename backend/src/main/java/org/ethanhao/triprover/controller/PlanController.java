a547eb5fb0075eb020cdb8ab77e5e50ccbdb46d2
Ethan Hao <hao.yz.ethan@gmail.com>
2024-10-05 17:43:17 -0400

    adding logging

--- backend/src/main/java/org/ethanhao/triprover/controller/PlanController.java
+++ backend/src/main/java/org/ethanhao/triprover/controller/PlanController.java
@@ -2,8 +2,9 @@ package org.ethanhao.triprover.controller;
 
 import org.ethanhao.triprover.domain.LoginUser;
 import org.ethanhao.triprover.domain.Plan;
-import org.ethanhao.triprover.domain.PlanPlace;
 import org.ethanhao.triprover.service.PlanService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -13,13 +14,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/plan")
 public class PlanController {
 
     private final PlanService planService;
+    private static final Logger logger = LoggerFactory.getLogger(PlanController.class);
 
     @Autowired
     public PlanController(PlanService planService) {
@@ -34,7 +35,10 @@ public class PlanController {
     ) {
         LoginUser loginUser = (LoginUser) authentication.getPrincipal();
         Long userId = loginUser.getUser().getId();
+
+        logger.info("User {} fetching plan {}", userId, planId);
         if (!planService.isUserAuthorized(planId, userId)) {
+            logger.info("User {} is not authorized to fetch plan {}", userId, planId);
             return ResponseEntity.status(403).build(); // Forbidden
         }
 
