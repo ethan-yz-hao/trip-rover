a209c7db6136984b17df4c103af51697ee24dc60
Ethan Hao <hao.yz.ethan@gmail.com>
2024-10-07 18:29:07 -0400

    update script for dummy data

--- backend/script/TestData.sql
+++ backend/script/TestData.sql
@@ -20,23 +20,23 @@ INSERT INTO sys_role_menu (role_id, menu_id) VALUES
 
 -- Associating Users with Roles
 INSERT INTO sys_user_role (user_id, role_id) VALUES
-    ((SELECT id FROM sys_user WHERE user_name = 'admin'), (SELECT id FROM sys_role WHERE role_name = 'Administrator')),
-    ((SELECT id FROM sys_user WHERE user_name = 'johndoe'), (SELECT id FROM sys_role WHERE role_name = 'User')),
-    ((SELECT id FROM sys_user WHERE user_name = 'ethanhao'), (SELECT id FROM sys_role WHERE role_name = 'User'));
+                                                 ((SELECT id FROM sys_user WHERE user_name = 'admin'), (SELECT id FROM sys_role WHERE role_name = 'Administrator')),
+                                                 ((SELECT id FROM sys_user WHERE user_name = 'johndoe'), (SELECT id FROM sys_role WHERE role_name = 'User')),
+                                                 ((SELECT id FROM sys_user WHERE user_name = 'ethanhao'), (SELECT id FROM sys_role WHERE role_name = 'User'));
 
 -- Inserting Plans
 INSERT INTO plans (plan_name, version) VALUES
                                            ('Trip to New York', 0),
                                            ('Road Trip across California', 0);
 
--- Inserting PlanPlaces with Manually Generated UUIDs for place_id
-INSERT INTO plan_places (plan_id, sequence_number, place_id) VALUES
-                                                                                                ((SELECT plan_id FROM plans WHERE plan_name = 'Trip to New York'), 0, '550e8400-e29b-41d4-a716-446655440000'),
-                                                                                                ((SELECT plan_id FROM plans WHERE plan_name = 'Trip to New York'), 1, '550e8400-e29b-41d4-a716-446655440001'),
-                                                                                                ((SELECT plan_id FROM plans WHERE plan_name = 'Trip to New York'), 2, '550e8400-e29b-41d4-a716-446655440002'),
-                                                                                                ((SELECT plan_id FROM plans WHERE plan_name = 'Road Trip across California'), 0, '550e8400-e29b-41d4-a716-446655440003'),
-                                                                                                ((SELECT plan_id FROM plans WHERE plan_name = 'Road Trip across California'), 1, '550e8400-e29b-41d4-a716-446655440004'),
-                                                                                                ((SELECT plan_id FROM plans WHERE plan_name = 'Road Trip across California'), 2, '550e8400-e29b-41d4-a716-446655440005');
+-- Inserting PlanPlaces
+INSERT INTO plan_places (plan_id, sequence_number, place_id, google_place_id, stay_seconds) VALUES
+                                                                                                ((SELECT plan_id FROM plans WHERE plan_name = 'Trip to New York'), 0, '550e8400-e29b-41d4-a716-446655440000', 'ChIJp9d9hbpZwokRCM5ylxp7N6k', 3600),
+                                                                                                ((SELECT plan_id FROM plans WHERE plan_name = 'Trip to New York'), 1, '550e8400-e29b-41d4-a716-446655440001', 'ChIJaXQRs6lZwokRY6EFpJnhNNE', 7200),
+                                                                                                ((SELECT plan_id FROM plans WHERE plan_name = 'Trip to New York'), 2, '550e8400-e29b-41d4-a716-446655440002', 'ChIJ-4MbiBlawokR5ixJ4E8Km5c', 5400),
+                                                                                                ((SELECT plan_id FROM plans WHERE plan_name = 'Road Trip across California'), 0, '550e8400-e29b-41d4-a716-446655440003', 'ChIJBQDT8fGej4ARtOyl34jt6Ro', 4800),
+                                                                                                ((SELECT plan_id FROM plans WHERE plan_name = 'Road Trip across California'), 1, '550e8400-e29b-41d4-a716-446655440004', 'ChIJneqLZyq7j4ARf2j8RBrwzSk', 6000),
+                                                                                                ((SELECT plan_id FROM plans WHERE plan_name = 'Road Trip across California'), 2, '550e8400-e29b-41d4-a716-446655440005', 'ChIJ_Yjh6Za1j4AR8IgGUZGDDTs', 3000);
 
 -- Associating Users with Plans via PlanUserRole (including specific roles)
 INSERT INTO plan_user_roles (plan_id, user_id, role) VALUES
