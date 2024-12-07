20d28d29bc48f44f129f498b3cd58fd02f27c1f1
Ethan Hao <hao.yz.ethan@gmail.com>
2024-12-07 15:11:13 -0500

    logout frontend

---
+++ frontend/app/components/Logout.tsx
@@ -0,0 +1,28 @@
+'use client';
+import React from 'react'
+import log from "@/app/log";
+
+const handleLogout = async () => {
+    try {
+        const response = await fetch('http://localhost:8080/api/user/logout', {
+            method: 'POST',
+            credentials: 'include',
+        });
+        
+        if (!response.ok) {
+            throw new Error(`Logout failed with status: ${response.status}`);
+        }
+        
+        log.log('Logged out successfully');
+    } catch (error) {
+        log.error('Error logging out:', error);
+    }
+}
+
+const Logout = () => {
+  return (
+    <button onClick={handleLogout}>Logout</button>
+  )
+}
+
+export default Logout
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,12 @@
 import PlanComponent from "@/app/components/PlanComponent";
 import LoginForm from "@/app/components/LoginForm";
+import Logout from "./components/Logout";
 
 export default function Home() {
     return (
         <>
             <LoginForm/>
+            <Logout/>
             <PlanComponent planId={1}/>
         </>
     );
